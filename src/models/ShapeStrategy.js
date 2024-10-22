5e8559d05c0db2917f13424e93153196d0a0c062
RobTO95 <roberio.teixeira.oliveira@gmail.com>
2024-10-21 21:07:25 -0300

    Adição das Strategy para criação de shapes default

--- src/models/ShapeStrategy.js
+++ src/models/ShapeStrategy.js
@@ -1,5 +1,45 @@
-class ShapeStrategy {
+export default class ShapeStrategy {
 	createShape(drawScreen, position) {
 		throw new Error("This method must be overridden by subclasses");
+	}
+}
+
+export class LShapeStrategy extends ShapeStrategy {
+	createShape(drawScreen, dimensions) {
+		const {
+			legLength1,
+			legLength2,
+			thickness1,
+			thickness2,
+			radius1,
+			radius2,
+			radius3,
+			radius4,
+		} = dimensions;
+		return new LShapedBeam(
+			drawScreen,
+			legLength1,
+			legLength2,
+			thickness1,
+			thickness2,
+			radius1,
+			radius2,
+			radius3,
+			radius4
+		);
+	}
+}
+
+export class TShapeStrategy extends ShapeStrategy {
+	createShape(drawScreen, dimensions) {
+		const { soulLength, soulThickness, flangeLength, flangeThickness } =
+			dimensions;
+		return new TShapeBeam(
+			drawScreen,
+			soulLength,
+			soulThickness,
+			flangeLength,
+			flangeThickness
+		);
 	}
 }
--- src/models/ShapesDefault.js
+++ src/models/ShapesDefault.js
@@ -1,5 +1,6 @@
 import CustomShape from "./CustomShape";
 
+// ---------------------------------------------------------------------------------------------------------
 export class LShapedBeam extends CustomShape {
 	constructor(
 		drawScreen,
@@ -13,6 +14,29 @@ export class LShapedBeam extends CustomShape {
 		radius4 = 0
 	) {
 		super(drawScreen);
+		this.setDimensions(
+			legLength1,
+			legLength2,
+			thickness1,
+			thickness2,
+			radius1,
+			radius2,
+			radius3,
+			radius4
+		);
+	}
+
+	// Método para atualizar as dimensões
+	setDimensions(
+		legLength1,
+		legLength2,
+		thickness1,
+		thickness2,
+		radius1,
+		radius2,
+		radius3,
+		radius4
+	) {
 		this.legLength1 = legLength1;
 		this.legLength2 = legLength2;
 		this.thickness1 = thickness1;
@@ -22,10 +46,17 @@ export class LShapedBeam extends CustomShape {
 		this.radius3 = radius3;
 		this.radius4 = radius4;
 
-		// this.anchor(0, 0);
+		// Redesenha o shape com as novas dimensões
+		this.redraw();
+	}
 
+	// Método para redesenhar o shape
+	redraw() {
+		// Limpa o desenho existente
+		this.clear();
+
+		// Redesenha o shape com as dimensões atuais
 		this.arc(this.radius, this.radius, this.radius, 180, 270, false);
-
 		this.line(this.legLength1, 0);
 		this.arc(
 			this.legLength1 - this.radius2,
@@ -43,7 +74,6 @@ export class LShapedBeam extends CustomShape {
 			180,
 			true
 		);
-		// this.line(this.thickness2, this.legLength2);
 		this.arc(
 			this.radius4 > 0 ? this.thickness2 - this.radius4 : this.thickness2,
 			this.legLength2 - this.radius4,
@@ -55,8 +85,14 @@ export class LShapedBeam extends CustomShape {
 		this.line(0, this.legLength2);
 		this.close();
 	}
+
+	// Método para limpar o desenho existente
+	clear() {
+		this.removePath(); // Remova o caminho anterior do SVG
+	}
 }
 
+// ---------------------------------------------------------------------------------------------------------
 export class TShapeBeam extends CustomShape {
 	constructor(
 		drawScreen,
@@ -66,20 +102,46 @@ export class TShapeBeam extends CustomShape {
 		flangeThickness
 	) {
 		super(drawScreen);
+		this.setDimensions(
+			soulLength,
+			soulThickness,
+			flangeLength,
+			flangeThickness
+		);
+	}
+
+	// Método para atualizar as dimensões
+	setDimensions(soulLength, soulThickness, flangeLength, flangeThickness) {
 		this.soulLength = soulLength;
-		this.flangeLength = flangeLength;
 		this.soulThickness = soulThickness;
+		this.flangeLength = flangeLength;
 		this.flangeThickness = flangeThickness;
+
+		// Redesenha o shape com as novas dimensões
+		this.redraw();
+	}
+
+	// Método para redesenhar o shape
+	redraw() {
+		// Limpa o desenho existente
+		this.clear();
+
+		// Redesenha o shape com as dimensões atuais
 		this.anchor(0, 0);
-		this.line(soulThickness / 2, 0);
-		this.line(soulThickness / 2, soulLength);
-		this.line(flangeLength / 2, soulLength);
-		this.line(flangeLength / 2, soulLength + flangeThickness);
-		this.line(0, soulLength + flangeThickness);
-		this.line(-(flangeLength / 2), soulLength + flangeThickness);
-		this.line(-(flangeLength / 2), soulLength);
-		this.line(-(soulThickness / 2), soulLength);
-		this.line(-(soulThickness / 2), 0);
+		this.line(this.soulThickness / 2, 0);
+		this.line(this.soulThickness / 2, this.soulLength);
+		this.line(this.flangeLength / 2, this.soulLength);
+		this.line(this.flangeLength / 2, this.soulLength + this.flangeThickness);
+		this.line(0, this.soulLength + this.flangeThickness);
+		this.line(-(this.flangeLength / 2), this.soulLength + this.flangeThickness);
+		this.line(-(this.flangeLength / 2), this.soulLength);
+		this.line(-(this.soulThickness / 2), this.soulLength);
+		this.line(-(this.soulThickness / 2), 0);
 		this.close();
+	}
+
+	// Método para limpar o desenho existente
+	clear() {
+		this.removePath(); // Remove o caminho anterior do SVG
 	}
 }
